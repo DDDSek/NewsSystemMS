28b936fb9ad29581dd10f6243ea7d14a078e50b4
ggfd <sekulsfml@gmail.com>
2020-11-02 19:00:55 +0200

    Separate the projects and preparing for Microservices

---
+++ Common/Common.Domain/Models/ValueObject.Specs.cs
@@ -0,0 +1,41 @@
+﻿namespace NewsSystem.Domain.Common.Models
+{
+    using FluentAssertions; 
+    using Xunit;
+
+    public class ValueObjectSpecs
+    {
+        [Fact]
+        public void ValueObjectsWithEqualPropertiesShouldBeEqual()
+        {
+            // Arrange
+            var first = new TestValueObject();
+            var second = new TestValueObject();
+
+            // Act 
+            var result = first == second;  // equality of the objects does it work ok
+
+            // Arrange
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ValueObjectsWithDifferentPropertiesShouldNotBeEqual()
+        {
+            // Arrange
+            var first = new TestValueObject();
+            var second = new TestValueObject();
+
+            // Act
+            var result = first == second;
+
+            // Arrange
+            result.Should().BeFalse();
+        }
+
+        private class TestValueObject : ValueObject
+        {
+
+        }
+    }
+}
